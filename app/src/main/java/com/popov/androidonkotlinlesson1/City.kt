81f41d49df97b778020352388ff7fed5e773c218
ArtemPopov18 <Arshawin1991gmail.com>
2022-06-25 17:48:09 +0700

    Сделал задание с Data Class и циклами!

---
+++ app/src/main/java/com/popov/androidonkotlinlesson1/City.kt
@@ -0,0 +1,3 @@
+package com.popov.androidonkotlinlesson1
+
+data class City(val name: String, var age: Int)
--- app/src/main/java/com/popov/androidonkotlinlesson1/MainActivity.kt
+++ app/src/main/java/com/popov/androidonkotlinlesson1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.popov.androidonkotlinlesson1
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var clickСounter: ClickСounter = ClickСounter()
         val counterOutput = findViewById<TextView>(R.id.counter_output)
+        val textDataClass1 = findViewById<TextView>(R.id.textDataClass1)
+        val textDataClass2 = findViewById<TextView>(R.id.textDataClass2)
+
         counterOutput.text = clickСounter.count.toString()
         findViewById<AppCompatButton>(R.id.button_1).setOnClickListener(object :
             View.OnClickListener {
@@ -20,5 +24,19 @@ class MainActivity : AppCompatActivity() {
                 counterOutput.text = clickСounter.count.toString()
             }
         })
+        val city1: City = City("Самара", 436)
+        val city2: City = city1.copy("Tomsk", 418)
+        textDataClass1.text = "${city1.name} ${city1.age.toString()}"
+        textDataClass2.text = "${city2.name} ${city2.age.toString()}"
+
+        fun testIf(){
+            if (city1.age > city2.age){
+                Log.d("@@@", "${city1.name} раньше основали")
+            } else {
+                Log.d("@@@", "${city2.name} раньше основали")
+            }
+        }
+
+        testIf()
     }
 }
